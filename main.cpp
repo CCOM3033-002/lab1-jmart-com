3132ebcc48b8ee5b24f0f87491f715eb79a6ef10
Javier A. Martinez <javier.martinez36@upr.edu>
2025-02-08 04:09:17 +0000

    main

--- main.cpp
+++ main.cpp
@@ -25,25 +25,25 @@ int main()
 
     string mName;
 
-    int mTicketsAdults;
-    int mTicketsChildren;
+    double mTicketsAdults;
+    double mTicketsChildren;
 
-    int mGrossAdults;
-    int mGrossChildren;
+    double mGrossAdults;
+    double mGrossChildren;
 
-    int mGrossProfit;
-    int mNetProfit;
+    double mGrossProfit;
+    double mNetProfit;
 
-    int mDistTax;
+    double mDistTax;
 
     // Recibir Datos de Usuario
 
     cout << "Enter the name of the movie: ";
     getline(cin, mName);
     cout << "Enter the amount of adult tickets sold: ";
-    cin >> mTicketsAdults >> endl;
+    cin >> mTicketsAdults;
     cout << "Enter the amount of child tickets sold: ";
-    cin >> mTicketsChildren >> endl;
+    cin >> mTicketsChildren;
 
     // Hacer calculos
 
@@ -59,11 +59,10 @@ int main()
 
     cout << endl;
 
-    cout << "Movie Name: " << setw(44) << mName << endl;
-    cout << "Adult Tickets Sold: " << setw(13) << mTicketsAdults << endl;
-    cout << "Child Tickets Sold: " << setw(13) << mTicketsChildren << endl;
-    
-    cout << "Gross Box Office Profit: " << setw(4) << "$" << setw(9) << mGrossProfit << endl;
+    cout << "Movie Name: " << setw(27) << mName << endl;
+    cout << "Adult Tickets Sold: " << setw(15) << mTicketsAdults << endl;
+    cout << "Child Tickets Sold: " << setw(15) << mTicketsChildren << endl;
+    cout << "Gross Box Office Profit: " << setw(4) << "$" << setw(9) << setprecision(2) << fixed << mGrossProfit << endl;
     cout << "Net Box Office Profit: " << setw(6) << "$" << setw(9) << mNetProfit << endl;
     cout << "Amount Paid to Distributor: " << "$" << setw(9) << mDistTax << endl;
 
